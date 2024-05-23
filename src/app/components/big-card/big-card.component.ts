2fce063a385575de1b99b637e8dac8451c6abe0a
Willans Junes <willans@tuyaymia.com>
2024-05-23 14:28:44 -0300

    Adicionando dinamica  a los  elementos y estilos de los mismos y finalizando footer.

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css','./big-card.responsive.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent  {
+
+@Input()
+  photoCover:string = "https://dcreations.es/storage/files/1/blog/angular/novedades-angular-17/novedades-angular-17.webp"
+  cardTitle:string = "Fué lanzada la nueva version de angular"
+  cardDescription:string = " Confíe en las herramientas de internacionalización de Angular, la seguridad y la accesibilidad para construir todos en todo el mundo."
 
 }
