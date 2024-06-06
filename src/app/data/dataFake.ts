3a41a8a8ef6b89d06e448277a6b39204fb7e1ef9
Willans Junes <willans@tuyaymia.com>
2024-06-06 16:34:42 -0300

    Commit de archivo app bancaria para realizar corrrecciones y mejoras enpróxima actualización

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo": "aaa",
+        "title": "Lanzado nuevo film",
+        "description": "Aqui ahora"
+    },
+    {
+        "id":2,
+        "photo": "aaa",
+        "title": "Lanzado nuevo film",
+        "description": "Aqui ahora"
+    }
+]
